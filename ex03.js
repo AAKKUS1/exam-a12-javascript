24c80146698f7b061c02d56480f0cc67190f0dbb
AAKKUS1 <aakkus@edenschool.fr>
2025-01-23 11:16:51 +0100

    feat(test)code-limits

--- ex03.js
+++ ex03.js
@@ -1,6 +1,7 @@
 function estNegatif(nombre) {
-	return nombre < 0
+	return nombre < 0 || nombre === 0
 }
 
 console.log(estNegatif(-3)); 
 console.log(estNegatif(5));
+console.log(estNegatif(0));
--- ex04.js
+++ ex04.js
@@ -1,6 +1,8 @@
 function dansPlage(nombre, min, max) {
-	return nombre > min && nombre < max
+	return nombre > min && nombre < max || nombre == min || nombre == max
 }
 
 console.log(dansPlage(5, 1, 10)); 
 console.log(dansPlage(15, 1, 10));
+console.log(dansPlage(10, 1, 10)); 
+console.log(dansPlage(1, 1, 10));
